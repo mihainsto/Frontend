098a0aaaad7df92eddfc26cddf9a79553084b450
mihainsto <44275480+mihainsto@users.noreply.github.com>
2019-11-16 15:38:25 +0200

    Revert "added template pages"
    This reverts commit 019e23780d12b1283e95d0b8906b637e144acf82.

--- script/shared.js
+++
@@ -1,9 +0,0 @@
-function redirectTOHomePage() {
-  location.replace("index.html")
-}
-function redirectToRTX2080Super(){
-  location.replace("rtx2080Super.html")
-}
-function redirectTOWhereToBuy(){
-  location.replace("buy.html")
-}
